4e8f841a10228d57d01439f892255d7294fcf3fe
Oystein Olsen <mail@oysteinolsen.com>
2015-07-20 00:20:43 +0200

    Get exif info

--- server.py
+++ server.py
@@ -2,11 +2,12 @@ from flask import Flask, request
 from flask import render_template
 from werkzeug import secure_filename
 import os
+import exif
 app = Flask(__name__)
 
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['JPG', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -33,6 +34,22 @@ def upload_file():
             return "Success!"
     return 0
 
+@app.route("/json/<directory>")
+def create_json(directory):
+    folder_path = os.path.join('uploads', directory)
+    json_data = []
+    for filename in os.listdir(folder_path):
+        fullpath = os.path.join(folder_path, filename)
+        if os.path.isfile(fullpath):
+            print fullpath
+            file_data = {}
+            file_data["filename"] = fullpath
+            lat_lon = exif.get_gps_radians(fullpath)
+            if lat_lon:
+                file_data["latitude"] = lat_lon["latitude"]
+                file_data["longitude"] = lat_lon["longitude"]
+            print file_data
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
